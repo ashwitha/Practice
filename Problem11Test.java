987e27e07146735fc4bc6e73aa3366a89a095ded
ashwitha <ashwithadr@orbismedia.in>
2011-02-09 12:52:23 +0530

    first commit

---
+++ Problem11Test.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+
+public class Problem11Test extends TestCase 
+{
+	public void testToFindProductOfNumbers()
+	{
+		assertEquals(70600674, new Problem11().productofnumbers());
+	}
+}
