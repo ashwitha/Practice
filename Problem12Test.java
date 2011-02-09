309649db20e49d38d6fa0fc56ebf353fb6a18830
ashwitha <ashwithadr@orbismedia.in>
2011-02-09 14:47:48 +0530

    commit files

---
+++ Problem12Test.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+
+public class Problem12Test extends TestCase
+{
+  public void testToFindFirstTriangleToHaveFiveHundredDivisors()
+  {
+	  assertEquals(76576500,new Problem12().FirstTriangleToHaveFiveHundredDivisors());
+  }
+}
