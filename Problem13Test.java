27cd9571f8ebf934e4fe4610ba7dcfb3d7c8b653
ashwitha <ashwithadr@orbismedia.in>
2011-02-10 10:40:13 +0530

    commit this

---
+++ Problem13Test.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+
+public class Problem13Test extends TestCase
+{
+public void testToFindSumOfDigits()
+{
+	assertEquals(262660, new Problem13().SumOfDigits());
+}
+}
---
+++ Problem14Test.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+
+
+public class Problem14Test extends TestCase 
+{
+public void testToFindLongestSequence()
+{
+	assertEquals(837799, new Problem14().longestsequence());
+}
+}
